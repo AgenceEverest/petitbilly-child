63bd7c51dd17bef59430f48277a0dd923d3136fe
Agence Everest <contact@agence-everest.com>
2022-06-23 16:34:22 +0200

    corrections import et génération du thème enfant

--- functions.php
+++ functions.php
@@ -5,8 +5,8 @@ function styles2_css()
     wp_enqueue_style( 'child-style', get_stylesheet_uri(),
         array( 'parenthandle' )
     );
-    // Chargement de main2.css pour le thème enfant.
-    wp_enqueue_style('main2', get_stylesheet_directory_uri() . '/css/main2.css', array(), filemtime(get_stylesheet_directory_uri() . '/css/main2.css'), false);
+    // Chargement de mainchild.css pour le thème enfant.
+    wp_enqueue_style('mainchild', get_stylesheet_directory_uri() . '/css/mainchild.css', array(), filemtime(get_stylesheet_directory_uri() . '/css/mainchild.css'), false);
 }
 
 //la priorité à 99, c'est pour que main2.css soit chargé après main.css, de cette manière les règles CSS du thème enfant peuvent écraser celles du thème parent.
