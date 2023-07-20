84ba7ce57853cf69785a50796efb01e246759557
Nicolas TROADEC - USEWEB <nicolas.troadec@useweb.com>
2023-07-20 17:41:29 +0200

    update

--- inc/blocks/block-1-colonne-custom.php
+++ inc/blocks/block-1-colonne-custom.php
@@ -39,6 +39,7 @@ endif;
             endif; ?>
 ">
     <?php if ($vague_au_dessus_du_bloc) : ?>
+        TESTGR
         <?= showSvg(get_stylesheet_directory_uri() . '/svg/vague.svg') ?>
     <?php endif; ?>
     <div <?php if ($ajouter_un_id_pour_le_css) : echo " id='" . $ajouter_un_id_pour_le_css . "'";
