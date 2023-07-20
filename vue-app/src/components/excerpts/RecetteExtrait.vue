15964c966fb80a6e802839f8dd6b43189104acad
Nicolas TROADEC - USEWEB <nicolas.troadec@useweb.com>
2023-07-20 09:42:47 +0200

    update

--- vue-app/src/components/excerpts/RecetteExtrait.vue
+++ vue-app/src/components/excerpts/RecetteExtrait.vue
@@ -54,13 +54,35 @@ export default {
         :key="taxo.id"
       >
         <template v-if="taxo && taxo.length">
-          <span
-            v-for="term in taxo"
-            :key="term.id"
-            :class="'term term-' + index"
-          >
-            {{ term.name }}
-          </span>
+          <div class="term-wrapper">
+            <span
+              v-for="term in taxo"
+              :key="term.id"
+              :class="'term term-' + index"
+            >
+              {{ term.name }}
+            </span>
+            <svg
+              class="term-bg"
+              xmlns="http://www.w3.org/2000/svg"
+              width="88.686"
+              height="38.778"
+              viewBox="0 0 88.686 38.778"
+            >
+              <g
+                id="Groupe_1651"
+                data-name="Groupe 1651"
+                transform="translate(-14.46 9.184)"
+              >
+                <path
+                  id="Tracé_342"
+                  data-name="Tracé 342"
+                  d="M25.743-.028C46.713-1.445,47,.568,61.609,1.985a130,130,0,0,0,22.921.475c.138.069,0,35.806,0,35.806H62.536c-13.372,0-20.435-1.109-33.856-2.016C7.71,34.834,7.983,39.736.235,32.083s-3.4-25.731,0-30.1S4.773,1.388,25.743-.028Z"
+                  transform="translate(18.556 -8.673)"
+                />
+              </g>
+            </svg>
+          </div>
         </template>
       </template>
       <template v-if="cpt.hasOwnProperty('_embedded')">
