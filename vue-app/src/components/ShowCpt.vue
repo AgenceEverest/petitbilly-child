b3f1aa54d32afb6616f9b74a9773e62a6515cb03
Nicolas TROADEC - USEWEB <nicolas.troadec@useweb.com>
2023-07-21 16:46:06 +0200

    update

--- vue-app/src/components/ShowCpt.vue
+++ vue-app/src/components/ShowCpt.vue
@@ -391,7 +391,7 @@ export default {
       <div @click="incrementmaxDisplayable" v-if="hasMoreContent" class="load-more">
         {{ dataJson.load_more_text }}
       </div>
-      <svg @click="incrementmaxDisplayable" class="load-more-svg" xmlns="http://www.w3.org/2000/svg" width="207.852" height="49.779" viewBox="0 0 207.852 49.779">
+      <svg @click="incrementmaxDisplayable" v-if="hasMoreContent" class="load-more-svg" xmlns="http://www.w3.org/2000/svg" width="207.852" height="49.779" viewBox="0 0 207.852 49.779">
         <path class="load-more-path" id="Tracé_342" data-name="Tracé 342"
           d="M65.823.109c49.139-1.819,49.813.766,84.042,2.584S179.388.16,194.784,7.383s7.607,26.36,1.449,33.947-12.86,7.938-44.195,7.938-47.884-1.424-79.333-2.588c-49.139-1.819-48.5,4.474-66.654-5.35s-7.97-33.03,0-38.637S16.684,1.927,65.823.109Z"
           transform="translate(4.095 0.511)" fill="#c5191b" />
