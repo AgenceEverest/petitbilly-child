da79771c6a20fb53b8fe47c06319d91a66b54561
Nicolas TROADEC - USEWEB <nicolas.troadec@useweb.com>
2023-07-24 14:33:19 +0200

    update

--- inc/blocks/block-plus-informations.php
+++ inc/blocks/block-plus-informations.php
@@ -3,15 +3,15 @@
 /**
  * Block Name: block-plus-informations   
  *
- * This is the template that displays the 1-colonnes-texte block.
+ * This is the template that displays the block-plus-informations   
  */
 if (have_rows('block_plus_informations')) : the_row(); // il s'agit du nom du champ dans ACF qui contient les sous-champs
     $cb_ajouter_une_classe_css = get_sub_field('cb_ajouter_une_classe_css');
     $ajouter_un_id_pour_le_css = get_sub_field('ajouter_un_id_pour_le_css');
     $contenu_texte = get_sub_field('contenu_texte');
     $texte_call_to_action = get_sub_field('texte_call_to_action');
-    $url_page_contact = get_field('url_du_bouton_contact', 'option');
-    $url_page_contact = $url_page_contact ? get_permalink($url_page_contact->ID) : '';
+    $url_page_contact_value = get_field('url_de_la_page_contact', 'option');
+    $url_page_contact = $url_page_contact_value ? get_permalink($url_page_contact_value->ID) : '';
 endif;
 ?>
 <div class="margin_section_top  margin_section_bottom">
