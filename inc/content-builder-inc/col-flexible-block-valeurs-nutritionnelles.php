2db1f5b9bfbdb154f43be71a98b599bc89c2da33
Nicolas TROADEC - USEWEB <nicolas.troadec@useweb.com>
2023-07-28 09:44:31 +0200

    update

--- inc/content-builder-inc/col-flexible-block-valeurs-nutritionnelles.php
+++ inc/content-builder-inc/col-flexible-block-valeurs-nutritionnelles.php
@@ -2,6 +2,7 @@
     <?php
     $titre_valeurs_nutritionnelles = get_sub_field('titre_valeurs_nutritionnelles');
     $sousTitreValeurs = get_sub_field('sous_titre_valeurs_nutritionnelles'); ?>
+    $unite_grammage = get_sub_field('unite_grammage'); ?>
     <h2><?= $titre_valeurs_nutritionnelles ?></h2>
     <p><?= $sousTitreValeurs ?></p>
     <?php if (have_rows('tableau_valeurs_nutritionnelles')) : ?>
@@ -10,7 +11,7 @@
                 <li><span class="valeur-nutritionnelle-nom">
                         <?= get_sub_field('valeur_nutritionnelle_nom'); ?>
                     </span><span class="valeur-nutritionnelle">
-                        <?= get_sub_field('valeur_nutritionnelle') ?>
+                        <?= get_sub_field('valeur_nutritionnelle') ?><?= get_sub_field('unite_grammage') ?>
                     </span>
                 </li>
             <?php endwhile; ?>
