7fe5012f1118f423ba3da519767b6bafb7ca10ce
Nicolas TROADEC - USEWEB <nicolas.troadec@useweb.com>
2023-07-28 10:35:04 +0200

    update

--- inc/content-builder-inc/col-flexible-block-valeurs-nutritionnelles.php
+++ inc/content-builder-inc/col-flexible-block-valeurs-nutritionnelles.php
@@ -1,11 +1,12 @@
 <div class="valeurs-nutritionnelles">
     <?php
     $titre_valeurs_nutritionnelles = get_sub_field('titre_valeurs_nutritionnelles');
-    $sousTitreValeurs = get_sub_field('sous_titre_valeurs_nutritionnelles'); ?>
+    $sousTitreValeurs = get_sub_field('sous_titre_valeurs_nutritionnelles'); 
+    $first_element_green = get_sub_field('first_element_green'); ?>
     <h2><?= $titre_valeurs_nutritionnelles ?></h2>
     <p><?= $sousTitreValeurs ?></p>
     <?php if (have_rows('tableau_valeurs_nutritionnelles')) : ?>
-        <ul>
+        <ul class="<?= $first_element_green ? "first-element-green" : "" ?>">
             <?php while (have_rows('tableau_valeurs_nutritionnelles')) : the_row(); ?>
                 <li><span class="valeur-nutritionnelle-nom">
                         <?= get_sub_field('valeur_nutritionnelle_nom'); ?>
