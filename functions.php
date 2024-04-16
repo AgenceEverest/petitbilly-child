4dd63f46d52e24e0074fc07968f9cc2041d9634e
Agence Everest <contact@agence-everest.com>
2024-04-16 14:34:48 +0200

    desactive l'import ACF PHP block-3-colonnes du thème parent

--- functions.php
+++ functions.php
@@ -17,6 +17,7 @@ add_action('wp_enqueue_scripts', 'styles2_css', 99);
 // On désactive certains imports ACF PHP du thème parent.
 define('DISABLE_ACF_BLOCK_1_COLONNE', true);
 define('DISABLE_ACF_BLOCK_2_COLONNES', true);
+define('DISABLE_ACF_BLOCK_3_COLONNES', true);
 
 
 /**
